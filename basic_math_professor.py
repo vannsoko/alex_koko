9c87fcd7e943c5d0ab7c88450248c56ac89c1245
vannsoko <konstantin.vannson@gmail.com>
2023-11-26 23:03:16 +0100

    ajout basic_math_professor.py

--- basic_math_professor.py
+++ basic_math_professor.py
@@ -4,17 +4,44 @@ import time
 
 
 def get_level() -> int:
+    """
+        Returns the user-given level.
+
+        :return: The level as an integer, 1, 2, or 3.
+    """
     while True:
         level: str = input("Level (1-3): ")
         if level in {"1", "2", "3"}:
             return int(level)
 
 
-def generate_integer(level) -> list[list[int], list[int]]:
+def generate_integer(level: int) -> list[list[int], list[int]]:
+    """
+        Generates two lists of random integers based on the given level.
+
+        :param level: The level of the generated integers.
+        :return: A list containing two lists of integers.
+    """
     return [[random.randint(10 ** (level - 1), (10 ** level) - 1) for _ in range(10)] for _ in range(2)]
 
 
+def format_time(seconds: int | float) -> str:
+    """
+      Formats the given time in seconds into a string representation.
+
+      :param seconds: The time in seconds.
+      :return: A string representation of the formatted time.
+    """
+    mins, secs = divmod(seconds, 60)
+    return f"{int(mins)} min(s) et {int(secs)} secondes" if mins else f"{int(secs)} secondes"
+
+
 def main() -> None:
+    """
+        Runs the main logic of the basic math professor program.
+
+        :return: None
+    """
     mistakes: int = 0
     x, y = generate_integer(get_level())
 
@@ -33,12 +60,7 @@ def main() -> None:
             print(f"{x[i]} + {y[i]} = {x[i] + y[i]}")
     needed_time = time.time() - start_time
     print(f"{max(0, (10 - mistakes))}/10")
-    if needed_time < 60:
-        print(f"Tu as pris {round(time.time() - start_time)} secondes.")
-    else:
-        print(f"Tu as pris {needed_time // 60} min(s) et {needed_time % 60} secondes.")
-
-
+    print(f"Tu as pris {format_time(needed_time)}.")
 
 
 if __name__ == "__main__":
